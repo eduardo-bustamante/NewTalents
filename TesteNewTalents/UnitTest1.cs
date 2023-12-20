40fdad28df5d1900295024e83d39718397f3a25c
Eduardo Bustamante <edu.bustamante1989@gmail.com>
2023-12-20 09:52:01 -0300

    Adicionar arquivos de projeto.

---
+++ TesteNewTalents/UnitTest1.cs
@@ -0,0 +1,90 @@
+using NewTalents;
+
+namespace TesteNewTalents
+{
+    public class UnitTest1
+    {
+        public Calculadora construirClasse()
+        {
+            string data = "03/02/2023";
+            Calculadora calc = new Calculadora("03/02/2023");
+
+            return calc;
+
+        }
+
+        [Theory]
+        [InlineData(1,2,3)]
+        [InlineData(4, 5, 9)]
+        public void TesteSomar(int val1, int val2, int resultado)
+        {
+            Calculadora calc = construirClasse();
+
+            int resultadoCalculadora = calc.Somar(val1,val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 2)]
+        [InlineData(4, 5, 20)]
+        public void TesteMultiplicar(int val1, int val2, int resultado)
+        {
+            Calculadora calc = construirClasse();
+
+            int resultadoCalculadora = calc.Multiplicar(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(6, 2, 4)]
+        [InlineData(5, 5, 0)]
+        public void TesteSubtrair(int val1, int val2, int resultado)
+        {
+            Calculadora calc = construirClasse();
+
+            int resultadoCalculadora = calc.Subtrair(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(6, 2, 3)]
+        [InlineData(5, 5, 1)]
+        public void TesteDividir(int val1, int val2, int resultado)
+        {
+            Calculadora calc = construirClasse();
+
+            int resultadoCalculadora = calc.Dividir(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Fact]
+        public void TestarDivisaoPorZero()
+        {
+            Calculadora calc = construirClasse();
+
+            Assert.Throws<DivideByZeroException>(
+                ()=> calc.Dividir(3,0)
+                );
+        }
+        [Fact]
+        public void TestarHistorico()
+        {
+            Calculadora calc = construirClasse();
+
+            calc.Somar(1, 2);
+            calc.Somar(50, 42);
+            calc.Somar(10, 21);
+            calc.Somar(4, 6);
+
+            var lista = calc.Historico();
+
+            Assert.NotEmpty(lista);
+            Assert.Equal(3, lista.Count);
+        }
+
+    }
+}
